6cad6c2859d277a560e8ffa8c2765129e7585036
VedantShirgaonkar <vedxntshirgaonkar@gmail.com>
2025-06-17 19:15:43 +0530

    changes to webpage integration

--- app.py
+++ app.py
@@ -38,10 +38,10 @@ def predict_datapoint():
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
-        return render_template('home.html',results=results[0])
+        return render_template('home.html',results=int(round(results[0])))
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5001)        
+    app.run(host="0.0.0.0",port=5001,debug=True)        
 
 
